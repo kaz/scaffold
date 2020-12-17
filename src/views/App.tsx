29368e2d75dc2b19ed3654b969ffa240341febb4
Kazuki Sawada <kazuki@6715.jp>
2020-12-17 19:30:58 +0900

    mv files

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,18 +1,14 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import logo from "../../public/logo.svg";
+import "../App.css";
 
-interface AppProps {}
-
-function App({}: AppProps) {
-	// Create the count state.
+const Component = () => {
 	const [count, setCount] = useState(0);
-	// Create the counter (+1 every second).
 	useEffect(() => {
 		const timer = setTimeout(() => setCount(count + 1), 1000);
 		return () => clearTimeout(timer);
-	}, [count, setCount]);
-	// Return the App component.
+	});
+
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -31,6 +27,5 @@ function App({}: AppProps) {
 			</header>
 		</div>
 	);
-}
-
-export default App;
+};
+export default Component;
