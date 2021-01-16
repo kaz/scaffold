bf756768904351f5020c9f52d705032322abc9af
Kazuki Sawada <kazuki@6715.jp>
2021-01-17 04:20:37 +0900

    update deps / snowpack config

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,19 +5,12 @@ module.exports = {
 		src: { url: "/dist" },
 	},
 	plugins: ["@snowpack/plugin-react-refresh", "@snowpack/plugin-typescript"],
-	installOptions: {
-		treeshake: true,
-	},
 	devOptions: {
 		open: "none",
 	},
-	buildOptions: {
-		clean: true,
-	},
-	experiments: {
-		optimize: {
-			bundle: true,
-			minify: true,
-		},
+	optimize: {
+		bundle: true,
+		minify: true,
+		treeshake: true,
 	},
 };
