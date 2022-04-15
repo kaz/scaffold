adf6ad3b9032252782444c901d830c75b5f377b2
Kazuki Sawada <kazuki@6715.jp>
2022-04-15 21:19:55 +0900

    update build config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,18 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	base: "./",
 	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				assetFileNames: "assets/[hash].[ext]",
+				chunkFileNames: "src/[hash].js",
+				entryFileNames: "src/[hash].js",
+			},
+		},
+	},
 	css: {
 		modules: {
-			generateScopedName: "[contenthash]",
+			generateScopedName: process.env.NODE_ENV != "development" ? "[md5:contenthash:base62:4]" : undefined,
 		},
 	},
 });
