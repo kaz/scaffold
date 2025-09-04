13b22b9c70abcf5532d725cf9b33ff127b75a38e
Kazuki Sawada <kazuki@6715.jp>
2025-09-04 15:23:04 +0900

    nits

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -9,6 +9,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+				<meta name="robots" content="noindex" />
 				<Meta />
 				<Links />
 			</head>
